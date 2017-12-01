a4e39687f63da425f76be1ffa2ab94a11bc363c6
Michael <mholtmann@ggc.edu>
2017-11-30 19:32:04 -0500

    Set up frame work

--- app/src/main/java/edu/ggc/michael/eightball/EightBall.java
+++ app/src/main/java/edu/ggc/michael/eightball/EightBall.java
@@ -1,13 +1,82 @@
 package edu.ggc.michael.eightball;
 
+import android.content.Context;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.SensorEventListener;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.util.Log;
 
-public class EightBall extends AppCompatActivity {
+public class EightBall extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private int count = 0;
+    private boolean flipped = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eight_ball);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        String sc = "Sensor changed";
+
+        //to see when flipped in development
+        if(count %10 == 0) {
+            Log.v(sc, "X " + event.values[0]);
+            Log.v(sc, "Y " + event.values[1]);
+            Log.v(sc, "Z " + event.values[2]);
+        }
+        count++;
+
+        if(event.values[2] < -2.5){
+            flipped = true;
+        }
+
+        if(flipped == true && event.values[2] > 0){
+            flipped = false;
+            ShowPrediction task = new ShowPrediction();
+            task.execute();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    class ShowPrediction extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            String post = "PostExe";
+            Log.v(post, "After doInBackground");
+        }
     }
 }
